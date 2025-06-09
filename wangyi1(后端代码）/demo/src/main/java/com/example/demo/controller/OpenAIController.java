c9cc580133f2bcbd0d67826ddcf15613e10d19e6
CXK <75512510+a099135828@users.noreply.github.com>
2025-06-09 19:58:33 +0800

    修改侧边栏显示且能正确跨域获取大模型返回

--- wangyi1(后端代码）/demo/src/main/java/com/example/demo/controller/OpenAIController.java
+++ wangyi1(后端代码）/demo/src/main/java/com/example/demo/controller/OpenAIController.java
@@ -3,11 +3,13 @@ package com.example.demo.controller;
 import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
+@CrossOrigin
 @RestController
 public class OpenAIController {
 
